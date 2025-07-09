9a768a99ca4c7f636e39d02e33a82674001dee3e
Refik Yalcin <ryalcin@uwaterloo.ca>
2025-07-09 15:44:41 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,8 +16,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    reg a,b;
+    wire out;
+  // A Very complex function 
+    always @(posedge clk) begin 
+        out = a & b; 
+    end
+
+
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[0],[1:7]  = out,0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
